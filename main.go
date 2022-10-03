a9c72081555b004b14394490e5f70d651a64b33e
Hannah.Alemu <hnnh.almu@gmail.com>
2022-10-03 15:14:26 +0100

    database file

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 )
-
+var sqliteDatabase *sql.DB
 func main() {
+//Open the database SQLite file and create the database table
+	database, err1 := sql.Open("sqlite3", "sqlite-database.db")
+	sqliteDatabase = database
+if err1 != nil {
+		log.Fatal(err1.Error())
+	}
+	//Defer the close
+	defer sqliteDatabase.Close()
+	
 	myhttp := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./."))
 	myhttp.Handle("/", http.StripPrefix("", fs))
