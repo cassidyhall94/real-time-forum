441e1611762176c267dacef2277978d1510e7f2c
Hannah.Alemu <hnnh.almu@gmail.com>
2022-10-10 16:57:25 +0100

    authenticate

--- contentSocket.go
+++ contentSocket.go
@@ -17,7 +17,35 @@ type contentSocket struct {
 	template string
 }
 
+var contentUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var savedContentSockets []*contentSocket
+
+
+
 func contentSocketCreate(w http.ResponseWriter, r *http.Request) {
+
+		c1, err1 := r.Cookie("1st-cookie")
+	if err1 == nil && !Person.Accesslevel {
+		// first home page access 
+		c1.MaxAge = -1
+		http.SetCookie(w, c1)
+	}
+	_, err := r.Cookie("1st-cookie")
+	if err != nil && Person.Accesslevel {
+		// logged in and on 2nd browser
+		Person.CookieChecker = false
+	} else if err == nil && Person.Accesslevel {
+		// Original browser and logged in
+		Person.CookieChecker = true
+	} else {
+		// not logged in yet
+	Person.CookieChecker = false
+	}
+
 	fmt.Println("Content Socket Request")
 
 	defer func() {
@@ -67,6 +95,11 @@ func (i *contentSocket) pollContentWS() {
 			case "home":
 				if err := tpl.ExecuteTemplate(w, "home.template", nil); err != nil {
 					fmt.Printf("Home ExecuteTemplate error: %+v\n", err)
+					return
+				}
+			case "reg-log":
+				if err := tpl.ExecuteTemplate(w, "reg-log.template", nil); err != nil {
+					fmt.Printf("Reg-Log ExecuteTemplate error: %+v\n", err)
 					return
 				}
 			}
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 var sqliteDatabase *sql.DB
 func main() {
@@ -16,14 +19,24 @@ if err1 != nil {
 	}
 	//Defer the close
 	defer sqliteDatabase.Close()
-	
+
 	myhttp := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./."))
 	myhttp.Handle("/", http.StripPrefix("", fs))
 
+myhttp.HandleFunc("/home", mainHandler)
 	myhttp.HandleFunc("/chat", chatSocketCreate)
 	myhttp.HandleFunc("/content", contentSocketCreate)
 
 	fmt.Println("http://localhost:8080")
 	http.ListenAndServe(":8080", myhttp)
+
+	
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseGlob("index.html"))
+	if err := tpl.Execute(w, Person); err != nil {
+		http.Error(w, "No such file or directory: Internal Server Error 500", http.StatusInternalServerError)
+	}
 }
